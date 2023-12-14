734c723c778041eccf42b28056c5514bb4c301a8
Hrishik <raj222444666@gmail.com>
2023-12-14 23:23:37 +0530

    contect modified

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -38,7 +38,7 @@ export default function Home() {
                 <img className="sm:w-96 w-48" src="https://i.ibb.co/2M7rtLk/Remote1.png" alt="image2" />
             </div>
 
-            <h1 className="text-center text-2xl sm:text-5xl py-10 font-medium">Lorem Ipsum Yojo</h1>
+            <h1 className="text-center text-2xl sm:text-5xl py-10 font-medium">Let's build together</h1>
         </div>
     );
 }
